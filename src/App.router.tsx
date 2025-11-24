4d491787083aa29ccc1324878089722c4a2e1e5c
JhonveraDev <seguridad.veracontainer@gmail.com>
2025-11-24 17:17:59 -0500

    feat: initialize React project with Vite, TypeScript, and ESLint
    - Added package.json with dependencies for React, React Router, and development tools.
    - Included global and reset CSS styles for consistent styling across browsers.
    - Created main application structure with routing and basic components (NavBar, Footer, HomePage).
    - Configured TypeScript with strict settings and bundler mode for better development experience.
    - Set up Vite configuration for React application.

---
+++ src/App.router.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { HomePage } from './features/Home/pages/HomePage';
+import { NavBar } from './ui/layout/NavBar';
+import { Footer } from './ui/layout/Footer';
+
+
+
+export default function App() {
+  return (
+    <div className="main-container">
+      <div className="content-wrap">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div >
+  );
+}
+
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.router.tsx'
+
+import "./assets/styles/global.css";
+import "./assets/styles/reset.css";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
