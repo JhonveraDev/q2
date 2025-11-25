74c9f89caddf5c25a5d457ff48ef3b7303dd43a8
JhonveraDev <seguridad.veracontainer@gmail.com>
2025-11-25 12:00:12 -0500

    Refactor code structure for improved readability and maintainability

--- src/features/Home/components/PopularChoices.tsx
+++ src/features/Home/components/PopularChoices.tsx
@@ -1,7 +1,9 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import a1 from '../../../assets/images/home/home-popular-choice-01.png'
+import a1 from '../../../assets/images/home/home-popular-choice-01.png';
+import a2 from '../../../assets/images/home/home-popular-choice-02.png';
+import a3 from '../../../assets/images/home/home-popular-choice-03.png';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -10,13 +12,16 @@ import 'swiper/css/pagination';
 // import required modules
 import { Pagination } from 'swiper/modules';
 
-export const PopularChoices = () =>  {
+export const PopularChoices = () => {
   return (
     <div className='popular-choice-container'>
-      <h2>Popular choices</h2>
+      <h2 className='popular-choice-title'>Most popular choices</h2>
       <Swiper
+        centeredSlides={true}
+        loop={true}
+        initialSlide={1}
         slidesPerView={3}
-        spaceBetween={30}
+        spaceBetween={5}
         pagination={{
           clickable: true,
         }}
@@ -24,11 +29,11 @@ export const PopularChoices = () =>  {
         className="mySwiper"
       >
         <SwiperSlide><img src={a1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={a3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={a2} alt="" /></SwiperSlide>
         <SwiperSlide><img src={a1} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={a1} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={a1} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={a1} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={a1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={a2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={a3} alt="" /></SwiperSlide>
       </Swiper>
     </div>
   );
